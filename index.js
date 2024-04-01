350518fa02a73a6a099c1e5e34cac95569982bce
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-04-02 01:27:28 +0300

    done

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
+// function isPalindrome(word) {
+//   // Write your algorithm here
+// }
+
+// /* 
+//   Add your pseudocode here
+// */
+
+// /*
+//   Add written explanation of your solution here
+// */
+
+// // You can run `node index.js` to view these console logs
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
+
+// module.exports = isPalindrome;
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  function reverseString(word) {
+    return word.split("").reverse().join("");
+  }
+
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -23,3 +44,4 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
